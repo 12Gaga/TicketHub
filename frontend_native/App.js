5680ab3c4fbbb9c74da89e86a2d6f0e17c882f3a
Xero <79957627+Htet-Ahkar@users.noreply.github.com>
2026-03-17 17:37:52 +0700

    Style: change stauts bar color

--- frontend_native/App.js
+++ frontend_native/App.js
@@ -1,12 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Navigations from "./Configs/Navigations";
 import { useRef } from "react";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const navigationRef = useRef(null);
 
   return (
     <NavigationContainer ref={navigationRef}>
+      <StatusBar barStyle="dark-content" backgroundColor="#FFFFFF" />
       <Navigations navigationRef={navigationRef} />
     </NavigationContainer>
   );
